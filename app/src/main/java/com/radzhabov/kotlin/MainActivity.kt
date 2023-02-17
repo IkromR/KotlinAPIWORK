d40ade4db73fad85398ef6764ac38513c0c90ec4
Your Name <you@example.com>
2023-02-17 11:31:38 +0300

    Added all helder

--- app/src/main/java/com/radzhabov/kotlin/MainActivity.kt
+++ app/src/main/java/com/radzhabov/kotlin/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.radzhabov.kotlin.Model.Stations
 import com.radzhabov.kotlin.Model.Airlines
+import com.radzhabov.kotlin.Model.Airports
+import com.radzhabov.kotlin.Model.Cities
+import com.radzhabov.kotlin.Model.Counties
+import com.radzhabov.kotlin.Resource.ResourceHelper
+
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -15,17 +19,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Stations handlers
-        val jsonFileStringStations = ResourceHelperStations.getJsonDataFromAssetStations("/stations.json")
-        val gsonStations = Gson()
-        val listStationsType = object : TypeToken<Stations>() {}.type
-        val stations: Stations = gsonStations.fromJson(jsonFileStringStations, listStationsType)
+        val gson = Gson()
+
+        // Counties handlers
+        val jsonFileStringCounties = ResourceHelper.getJsonDataFromAsset("/counties.json")
+        val listCountiesType = object : TypeToken<Counties>() {}.type
+        val counties: Counties = gson.fromJson(jsonFileStringCounties, listCountiesType)
+
+        // Cities handlers
+        val jsonFileStringCities = ResourceHelper.getJsonDataFromAsset("/cities.json")
+        val listCitiesType = object : TypeToken<Cities>() {}.type
+        val cities: Cities = gson.fromJson(jsonFileStringCities, listCitiesType)
 
         // Airlines handlers
-        val jsonFileStringAirlines = ResourceHelperAirlines.getJsonDataFromAssetAirlines("/airlines.json")
-        val gsonAirlines = Gson()
+        val jsonFileStringAirlines = ResourceHelper.getJsonDataFromAsset("/airlines.json")
         val listAirlinesType = object : TypeToken<Airlines>() {}.type
-        val airlines: Airlines = gsonAirlines.fromJson(jsonFileStringAirlines, listAirlinesType)
+        val airlines: Airlines = gson.fromJson(jsonFileStringAirlines, listAirlinesType)
 
+        // Airports handlers
+        val jsonFileStringAirports = ResourceHelper.getJsonDataFromAsset("/airports.json")
+        val listAirportsType = object : TypeToken<Airports>() {}.type
+        val airports: Airports = gson.fromJson(jsonFileStringAirports, listAirportsType)
     }
 }
--- app/src/main/java/com/radzhabov/kotlin/Resource/ResourceHelper.kt
+++ app/src/main/java/com/radzhabov/kotlin/Resource/ResourceHelper.kt
@@ -1,4 +1,4 @@
-package com.radzhabov.kotlin
+package com.radzhabov.kotlin.Resource
 
 import java.io.BufferedReader
 import java.io.IOException
@@ -6,14 +6,13 @@ import java.io.InputStream
 import java.io.InputStreamReader
 
 
-class ResourceHelperAirlines {
-
+class ResourceHelper {
     companion object {
-        fun getJsonDataFromAssetAirlines(resourcePath: String): String {
+        fun getJsonDataFromAsset(resourcePath: String): String {
             val result = StringBuilder()
 
             try {
-                val input  = ResourceHelperAirlines::class.java.getResourceAsStream(resourcePath) as InputStream
+                val input  = ResourceHelper::class.java.getResourceAsStream(resourcePath) as InputStream
                 if (input != null) {
                     val reader = BufferedReader(InputStreamReader(input))
                     var string: String? = reader.readLine()
@@ -27,7 +26,6 @@ class ResourceHelperAirlines {
                 e.printStackTrace()
 
             }
-
             return result.toString()
         }
     }
